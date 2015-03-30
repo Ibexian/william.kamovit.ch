68d29f5d46cd5e7c55d0e8a672ccdc946e8354f2
Will Kamovitch <wkamovitch@mediamath.com>
2015-03-30 13:18:08 -0400

    Adding in the ISS tracker

--- js/mapbox.js
+++ js/mapbox.js
@@ -1,3 +1,16 @@
+//Set up our custom linked GEOJSON
+  function Marker(lat, long, url, symbol, title, id){
+    this.geometry = {};
+    this.properties = {};
+    this.geometry.coordinates = [lat,long];
+    this.geometry.type = 'Point';
+    this.properties.id = id;
+    this.properties['marker-color'] = '#505050';
+    this.properties['marker-size'] = 'medium';
+    this.properties['marker-symbol'] = symbol;
+    this.properties.title = title;
+    this.type = 'Feature';
+}
 var makemap = function (zoom) {
   var map = L.mapbox.map('map', 'wjkamovitch.map-hcqzgwpo', {detectRetina: true}).setView([43, 14.9], zoom);
         //disable map movement
@@ -12,10 +25,8 @@ var makemap = function (zoom) {
         $(".leaflet-right").remove();
 
       // Return the map 
-      return map;
-
-        
-}
+      return map;        
+};
 
 // Given a screen size, return a zoom level for mapbox.
 var adaptivezoom = function(size) {
@@ -31,29 +42,17 @@ function checkWidth() {
   var zoom = adaptivezoom(windowSize);
   map = makemap(zoom);
 
-  //Set up our custom linked GEOJSON
-  function Marker(lat, long, url, symbol, title, id){
-    this.geometry = {};
-    this.properties = {};
-    this.geometry.coordinates = [lat,long];
-    this.geometry.type = 'Point';
-    this.properties['id'] = id;
-    this.properties['marker-color'] = '#505050';
-    this.properties['marker-size'] = 'medium';
-    this.properties['marker-symbol'] = symbol;
-    this.properties.title = title;
-    this.type = 'Feature';
-}
-var NewOriental = new Marker(112.938814,28.228209,"NewOriental", 'commercial', 'New Oriental', '3togyyo6');
-var Cambridge = new Marker(-71.253956,42.376727,'cambridge','commercial', 'The Cambridge Institute', 'lnxos0eb');
-var NKIDP = new Marker(-77.029716,38.8953,'NKIDP','commercial', 'North Korean International Documentation Project', 'xzsjc9cn');
-var PKU = new Marker(116.305611,39.987755, 'PKU', 'college', 'Peking University', 'xu37qi6o');
-var GWU = new Marker(-77.046272,38.899798, 'GWU', 'college', 'George Washington University', '0l5qibch');
-var SOAS = new Marker(-0.12889623641967773,51.52204224896724, 'SOAS', 'college', 'School of Oriental and African Studies', 'xdtpyk00');
-var geoJson = {
-    type: 'FeatureCollection',
-    features: [NewOriental, Cambridge, NKIDP, PKU, GWU, SOAS]
-    };
+
+  var NewOriental = new Marker(112.938814,28.228209,"NewOriental", 'commercial', 'New Oriental', '3togyyo6');
+  var Cambridge = new Marker(-71.253956,42.376727,'cambridge','commercial', 'The Cambridge Institute', 'lnxos0eb');
+  var NKIDP = new Marker(-77.029716,38.8953,'NKIDP','commercial', 'North Korean International Documentation Project', 'xzsjc9cn');
+  var PKU = new Marker(116.305611,39.987755, 'PKU', 'college', 'Peking University', 'xu37qi6o');
+  var GWU = new Marker(-77.046272,38.899798, 'GWU', 'college', 'George Washington University', '0l5qibch');
+  var SOAS = new Marker(-0.12889623641967773,51.52204224896724, 'SOAS', 'college', 'School of Oriental and African Studies', 'xdtpyk00');
+  var geoJson = {
+      type: 'FeatureCollection',
+      features: [NewOriental, Cambridge, NKIDP, PKU, GWU, SOAS]
+      };
   // Make the GeoJSON linkable in the same window
   map.markerLayer.setGeoJSON(geoJson);
   map.markerLayer.on('click', function(e) {
@@ -64,14 +63,35 @@ var geoJson = {
 }
 //Update zoom and recenter as window is resized
 var newsize = function(map) {
-    return function() {
-        var windowSize = $(window).width();
-        var zoomLevel = adaptivezoom(windowSize);
-        map.setView([43, 14.9], zoomLevel);
-    }    
-}                  
+  return function() {
+      var windowSize = $(window).width();
+      var zoomLevel = adaptivezoom(windowSize);
+      map.setView([43, 14.9], zoomLevel);
+  };    
+};
+
+var iss = function(map) {
+  var myIcon = L.icon({
+    iconUrl: '/img/rocket.png',
+    iconSize: [30, 30]
+  });
+  var issMarker = L.marker([-20,-20], {icon: myIcon, title:"ISS"}).addTo(map);
+  //now get the ISS Location
+  setInterval(function(){
+    $.ajax({
+      url: "http://api.open-notify.org/iss-now.json",
+      success: function(data) {
+        issMarker.setLatLng([data.iss_position.latitude, data.iss_position.longitude]);
+      },
+      dataType: "jsonp"
+    });
+  }, 2000);
+
+
+};                  
 
 $(document).ready(function() {
     var map = checkWidth();
+    iss(map);
     $(window).resize(newsize(map));
 });
